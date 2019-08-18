c50e3de830d83a355e7de8feb684b8718b88e432
jbathel <jessica.bathel@gmail.com>
2019-08-18 05:05:08 +0000

    Uploading initial file

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "holberton.h"
+
+
+
+#endif /* HOLBERTON_H */
